84fd47b3a87df7fc53786eb7a3fe756aa1c2ddd1
Salar Rahmanian <code@softinio.com>
2025-11-08 22:21:36 -0800

    add header comments to scripts

--- scripts/LaikaPreview.scala
+++ scripts/LaikaPreview.scala
@@ -5,6 +5,22 @@
 //> using dep org.http4s::http4s-ember-server:0.23.33
 //> using dep org.http4s::http4s-dsl:0.23.33
 
+/*
+ * Copyright 2024 Salar Rahmanian
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import laika.preview.*
 import laika.api.*
 import laika.format.*
